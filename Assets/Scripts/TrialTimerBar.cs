5e3c2c8610dd5aaadebd03f1441e8dcf997fd51e
Enrique Sanz <enriquesanz@gmail.com>
2017-07-18 22:54:02 +0200

    Final version

---
+++ Assets/Scripts/TrialTimerBar.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrialTimerBar : MonoBehaviour {
+
+	public float fullTime = 3.0f;
+	public float startingTime = 3.0f; 
+	public Slider Timer;
+
+	public bool timerActive = false;
+	public bool trialCanStart = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		TimerBar timebar = GameObject.Find ("TimerBar").GetComponent<TimerBar> ();
+
+		if (timerActive == true) {
+			startingTime -= Time.deltaTime * 60;
+			Timer.value = startingTime;
+
+			if (startingTime <= 0) {
+				trialCanStart = true;
+				GameObject.Find (timebar.currentTrial.ToString ()+"_0").GetComponent<Renderer> ().enabled = true;
+				startingTime = fullTime;
+			}
+		
+		}
+			
+	}
+}
